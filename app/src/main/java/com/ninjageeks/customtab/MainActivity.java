4a562bf3811d3dd468549a45d2bb623382f531ae
M4HDI-HK <mahdicsharp0098@gmail.com>
2020-11-20 13:16:55 +0330

    first commit

---
+++ app/src/main/java/com/ninjageeks/customtab/MainActivity.java
@@ -0,0 +1,80 @@
+package com.ninjageeks.customtab;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ColorStateList def;
+    TextView item1;
+    TextView item2;
+    TextView item3;
+    TextView select;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        item1 = findViewById(R.id.item1);
+        item2 = findViewById(R.id.item2);
+        item3 = findViewById(R.id.item3);
+        item1.setOnClickListener(this);
+        item2.setOnClickListener(this);
+        item3.setOnClickListener(this);
+        select = findViewById(R.id.select);
+        def = item2.getTextColors();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.item1){
+            select.animate().x(0).setDuration(100);
+            item1.setTextColor(Color.WHITE);
+            item2.setTextColor(def);
+            item3.setTextColor(def);
+        } else if (view.getId() == R.id.item2){
+            item1.setTextColor(def);
+            item2.setTextColor(Color.WHITE);
+            item3.setTextColor(def);
+            int size = item2.getWidth();
+            select.animate().x(size).setDuration(100);
+        } else if (view.getId() == R.id.item3){
+            item1.setTextColor(def);
+            item3.setTextColor(Color.WHITE);
+            item2.setTextColor(def);
+            int size = item2.getWidth() * 2;
+            select.animate().x(size).setDuration(100);
+        }
+    }
+}
